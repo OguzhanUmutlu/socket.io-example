d7aa0d029ff6d3ee4514473adaf936a0f3b84c13
Oğuzhan <64753457+OguzhanUmutlu@users.noreply.github.com>
2021-04-21 20:12:20 +0300

    Added file real-time

--- server.js
+++ server.js
@@ -10,10 +10,16 @@ io.on('connection', (socket) => {
     socket.on('results', msg => {
         io.emit('results', msg);
     });
+    socket.on('filetext', msg => {
+        io.emit('filetext', msg);
+    });
 });
 
 function addText(a) {
     io.emit("results", a);
+}
+function setFileText(a) {
+    io.emit("filetext", a);
 }
 
 app.get("/test", (req,res) => {
@@ -23,4 +29,8 @@ app.get("/test", (req,res) => {
 
 http.listen(3000, () => {
     console.log(`Socket.IO server running at http://localhost:3000/`);
+    setInterval(function(){
+        let file = require("fs").readFileSync("./some_folder/somefile.txt");
+        setFileText(file.toString());
+    }, 100)
 });
