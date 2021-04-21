7b226db5381172e2358fb6256f67c8efb05f4d4b
Oğuzhan <64753457+OguzhanUmutlu@users.noreply.github.com>
2021-04-21 20:01:01 +0300

    Good luck!

---
+++ server.js
@@ -0,0 +1,26 @@
+const app = require('express')();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', (socket) => {
+    socket.on('results', msg => {
+        io.emit('results', msg);
+    });
+});
+
+function addText(a) {
+    io.emit("results", a);
+}
+
+app.get("/test", (req,res) => {
+    addText("If your screen is on i should be in your screen.<br>");
+    res.send("Check screen!");
+})
+
+http.listen(3000, () => {
+    console.log(`Socket.IO server running at http://localhost:3000/`);
+});
